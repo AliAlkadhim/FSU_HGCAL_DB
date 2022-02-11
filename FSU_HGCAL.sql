036bce6ad60d5cd8388f811a5f2ba2502acee975
AliAlkadhim <aa18dg@my.fsu.edu>
2022-02-11 10:21:40 -0500

    progress on code generation

--- FSU_HGCAL.sql
+++ FSU_HGCAL.sql
@@ -1,2 +1,3 @@
 use FSU_HGCAL;
-create table Full_Sensor (  Sensor_ID varchar(16), Scratchpad_ID int );
+drop table Full_Sensor;
+create table Full_Sensor (  Sensor_ID varchar(32), Scratch_pad_ID varchar(32), Thick_ness varchar(32), P_Stop varchar(32), Oxide_type varchar(32), Flat_band_volt__V varchar(32), P_Stop_conc varchar(32), Proc varchar(32), Status_in_view_of_dicing_frame_removal varchar(32), Current_location varchar(32), RINSC_plan varchar(32), Pre_irrad_test_on_DF varchar(32), Pre_irrad_test_off_DF varchar(32), Sent_to_RINSC varchar(32), RINSC_irrad_round varchar(32), Post_irrad_test varchar(32) );
--- generate_sql.py
+++ generate_sql.py
@@ -1,6 +1,8 @@
 import csv
 import subprocess as sb
-filename = "Full_Sensor.csv"
+import re
+
+filename = "FSU_tables/Full_Sensor.csv"
 
 fields = []
 rows=[]
@@ -11,27 +13,34 @@ with open(filename, 'r') as csvfile:
     for row in csvreader:
         rows.append(row)
 
+new_fields = []
+for field in fields:
+    #we have to replace whitespace with _, field names have to be continuous
+    #we have to remove "-"" from field names and other characters
+    new_field = re.sub('\s+', '_', field)
+    new_field = re.sub('-', '', new_field)
+    new_field = re.sub('\.', '', new_field)
+    new_field = re.sub('\?', '', new_field)
+    new_field = re.sub('\(', '_', new_field)
+    new_field = re.sub('\)', '', new_field)
+    new_fields.append(new_field)
 
-
-
+fields = new_fields
 
 def generate_sql_code(fields, rows, Tablename, DB_sql_file):
     with open(DB_sql_file,'w') as sql_file:
         database = DB_sql_file[:-4] #name of the database without .sql
         
         sql_file.write('use %s' % database +';\n')
+        sql_file.write('drop table '+ Tablename + ';\n') #we drop it before creating it, otherwise it throws an error that table already exists
 
         sql_file.write('create table ' + Tablename + ' ( ')
         
-        for field in fields:
-            field.replace("\s", "_")
-            sql_file.write(' ' + field + ' varchar(16),')
+        for field in fields[:-1]:
+
+            sql_file.write(' ' + field + ' varchar(32),')
+        sql_file.write(' ' + fields[-1] + ' varchar(32)')
         sql_file.write(' );\n')
-
-
-
-
-
 
 
 
