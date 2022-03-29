a708e1981ea880a79ae8bf1f8ebdbf9eddddf005
Ali Al Kadhim <aa18dg@my.fsu.edu>
2022-03-29 21:49:32 +0200

    progress on auto

--- fsudb/updateDB.py
+++ fsudb/updateDB.py
@@ -15,7 +15,18 @@ def updateDB():
         cmd = 'ls -t $TMPFILES_DIR_IV/-/grading | head -n 1'
         latest_sensor = sb.Popen(cmd, shell=True,stdin=sb.PIPE, stdout=sb.PIPE, stderr=sb.STDOUT)
         latest_sensor = latest_sensor.stdout.read()
-        print('the latest sensor is', latest_sensor)
+        latest_sensor=latest_sensor[:-1]
+       
+	grading_results_for_latest_sensor_file = grading_dir + latest_sensor + '/grading_results.tex'
+        with open(grading_results_for_latest_sensor_file, 'r') as f:
+	    for line in f.readlines():
+	        print(line)
+
+
+
+
+
+
 if __name__ == '__main__':
     
     updateDB()
