c9bb85f27073eff2460f6f2a1e61027d3a5b7c3e
AliAlkadhim <aa18dg@my.fsu.edu>
2022-03-29 15:18:55 -0400

    auto

--- fsudb/updateDB.py
+++ fsudb/updateDB.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess as sb
 def updateDB():
     """automatically updates the FSUDB, given either IV or CV"""
     if sys.argv[1]:
@@ -9,9 +10,11 @@ def updateDB():
         return
     if sys.argv[1]== 'IV':
         TMPFILES_DIR_IV = os.environ('TMPFILES_DIR_IV')
-        grading_file = TMPFILES_DIR_IV +'/-/grading/'
-
-
+        
+        grading_dir = TMPFILES_DIR_IV +'/-/grading/'
+        cmd = 'ls -t $TMPFILES_DIR_IV | head -n 1'
+        latest_sensor = sb.Popen(cmd, shell=True)
+        print('the latest sensor is ', latest_sensor)
 if __name__ == '__main__':
     
     updateDB()
