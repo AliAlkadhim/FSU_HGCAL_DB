3e865f249003033482508c53168f69d6975970f0
AliAlkadhim <aa18dg@my.fsu.edu>
2022-03-31 10:12:53 -0400

    better parsing with df

--- fsudb/tests/updateDB.py
+++ fsudb/tests/updateDB.py
@@ -7,10 +7,13 @@ import pandas as pd
     #the long Ncell_with_1800_greaterthan_2_point_5_times_1600_and_1600_greaterthan_10nA_OR_1800_greaterthan_25nA_and_1600_lessthan_10nA
 
 
-HGC_HPK_Sensor_IV_Summary_LD_and_HD_fields = ['Sensor_ID', 'Scratch_pad_ID', 
+HGC_HPK_Sensor_IV_Summary_LD_and_HD_fields = [
+#identifiers
+'Sensor_ID', 'Scratch_pad_ID', 
 'Thick_ness', 'P_Stop', 
 'Oxide_type', 'Flat_band_volt_V', 'P_stop_conc', 
 'Proc', 'HD_Or_LD', 
+#results
 'I_tot_600V_lessthan_100mA', 
 'I_tot_800V_lessthan_2_point_5_times_I_tot_600V', 
 'Ncell_with_1800_greaterthan_2_point_5_times_1600', 
@@ -33,6 +36,8 @@ def updateDB():
         #return
     if sys.argv[1]== 'IV':
         #HGC_HPK_Sensor_IV_Summary_LD_and_HD.csv is the only table we need to worry about updating for IV
+
+        ############READ GRADING RESULTS
         TMPFILES_DIR_IV = os.environ['TMPFILES_DIR_IV']
 
         grading_dir = TMPFILES_DIR_IV +'/-/grading/'
@@ -42,6 +47,42 @@ def updateDB():
         latest_sensor=latest_sensor[:-1]
 
         grading_results_for_latest_sensor_file = grading_dir + latest_sensor + '/grading_results.tex'
+        with open(grading_results_for_latest_sensor_file,'r') as gr:
+            for line in gr.readlines():
+                #pass
+                if '<= 100 $\mu$A integrated' in line:
+                    criteria_I_tot_600V_lessthan_100mA = line.split('textcolor')[1].strip()
+                    if "Passed" in criteria_I_tot_600V_lessthan_100mA:
+                        criteria_I_tot_600V_lessthan_100mA = "Passed"
+                    else:
+                        criteria_I_tot_600V_lessthan_100mA = "Failed"
+                #I_tot_800V_lessthan_2_point_5_times_I_tot_600V looks like \item I800 < 2.5 x I600: \textcolor{green}{Passed}
+                if 'I800 < 2.5 x I600' in line:
+                    criteria_I_tot_800V_lessthan_2_point_5_times_I_tot_600V = line.split('textcolor')[1].strip()
+                    if "Passed" in criteria_I_tot_800V_lessthan_2_point_5_times_I_tot_600V:
+                        criteria_I_tot_800V_lessthan_2_point_5_times_I_tot_600V = "Passed"
+                    else:
+                        criteria_I_tot_800V_lessthan_2_point_5_times_I_tot_600V = "Failed"
+                
+                # Ncell_with_1800_greaterthan_2_point_5_times_1600 from the other tex file
+
+                #More_than_8_bad_cells_require_1_and_2 looks like \item Number of bad pads 0 <= 8 for full-sized sensors: \textcolor{green}{Passed}
+                if 'Number of bad pads 0 <= 8 for full-sized sensors' in line:
+                    criteria_More_than_8_bad_cells_require_1_and_2 = line.split('textcolor')[1].strip()
+                    if "Passed" in criteria_More_than_8_bad_cells_require_1_and_2:
+                        criteria_More_than_8_bad_cells_require_1_and_2 = "Passed"
+                    else:
+                        criteria_More_than_8_bad_cells_require_1_and_2 = "Failed"
+
+                # More_than_two_neighbor_cells_bad_require_1_and_2 looks like \item Allowed number of adjacent bad pads <= 2: \textcolor{green}{Passed}
+                if 'Allowed number of adjacent bad pads <= 2' in line:
+                    criteria_More_than_two_neighbor_cells_bad_require_1_and_2 = line.split('textcolor')[1].strip()
+                    if "Passed" in criteria_More_than_two_neighbor_cells_bad_require_1_and_2:
+                        criteria_More_than_two_neighbor_cells_bad_require_1_and_2 = "Passed"
+                    else:
+                        criteria_More_than_two_neighbor_cells_bad_require_1_and_2 = "Failed"
+
+                # print(criteria_I_tot_600V_lessthan_100mA)
         
 
         #scratchpad_ID has to be obtained from the database like HGC_HPK..., I think it has to be imput manually by the user!
@@ -71,6 +112,7 @@ def updateDB():
                 Temperature = row['Temperature']
 
         long_df = pd.DataFrame({
+        #DUT
         'Sensor_ID': [latest_sensor],
         'long_name':[long_name],
         'Size':[Size],
@@ -78,11 +120,13 @@ def updateDB():
         'Doping' :[Doping],
         'Thickness' : [Thickness],
         'P_Stop' : [Pstop],
-
-        # 'Date': [Date],
-        # 'Station':[Station],
-        # 'HVResistance':[HVResistance],
-        # 'Temperature':[Temperature],
+        #IV_MEASUREMENT
+        'Date': [Date],
+        'Station':[Station],
+        'HVResistance':[HVResistance],
+        'Temperature':[Temperature],
+        #GRADING_RESULTS
+        'I_tot_600V_lessthan_100mA':[criteria_I_tot_600V_lessthan_100mA]
 
         # 'Scratch_pad_ID':[None],
   
@@ -109,8 +153,8 @@ def updateDB():
                 #     'More_than_two_neighbor_cells_bad_require_1_and_2'}
                 # df = pd.DataFrame.from_dict(pd_dict)
     #print(long_df)
-    long_df.to_csv('new_results/long_df.csv')
-
+    #long_df.to_csv('new_results/long_df.csv')
+    print(criteria_More_than_two_neighbor_cells_bad_require_1_and_2)
 
 
 
