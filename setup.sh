57bdfda5e5adc2631ce45f09d0aeb8eeb76b3c9b
AliAlkadhim <aa18dg@my.fsu.edu>
2022-08-26 15:11:12 -0500

    upload test results in the uploaded wafers lilst

--- setup.sh
+++ setup.sh
@@ -3,25 +3,55 @@
 chmod -R 777 *
 ###export PATH=./:$PATH
 ### it's already in PYTHONPATH from the lcd setup.sh
-
+############ Source the lcd setup
 export ROOT_SOURCE="/software/ROOT_6_06_06/bin/thisroot.sh";
-
 export HEXPLOT_DIR="/software/hexplot/bin";
-
-export WORKFLOW_DIR="/home/lcd_hgcal_analysisworkflows";
-
+export WORKFLOW_DIR="/home/workflow/lcd_hgcal_analysisworkflows-v0.6";
 export DB_DIR="$WORKFLOW_DIR/database/csv";
 
 export PYTHONPATH=$WORKFLOW_DIR:$PYTHONPATH;
 
-export TMPFILES_DIR_IV="/home/OUTPUT_ALI/TMPFILES_DIR_IV"
-
-export SUMMARY_DIR_IV="/home/OUTPUT_ALI/SUMMARY_DIR_IV"
-
+export TMPFILES_DIR_IV="/home/output/hgsensor_iv"
+export SUMMARY_DIR_IV="/home/output/summaryhgsensor_iv"
 source $WORKFLOW_DIR/workflow_IV/setup.sh;
 
-export TMPFILES_DIR_CV="/home/OUTPUT_ALI/TMPFILES_DIR_CV"
-
-export SUMMARY_DIR_CV="/home/OUTPUT_ALI/SUMMARY_DIR_CV"
-
+export TMPFILES_DIR_CV="/home/output/hgsensor_cv"
+export SUMMARY_DIR_CV="/home/output/summaryhgsensor_cv" 
 source $WORKFLOW_DIR/workflow_CV/setup.sh;
+
+######Add input directory
+
+export INPUT_DIR="/home/input/"
+
+
+
+
+
+
+
+
+
+
+
+# Below is the exports when I work on it on my own docker of lcd_analysis_workflows on my ububtu machine
+# export ROOT_SOURCE="/software/ROOT_6_06_06/bin/thisroot.sh";
+
+# export HEXPLOT_DIR="/software/hexplot/bin";
+
+# export WORKFLOW_DIR="/home/lcd_hgcal_analysisworkflows";
+
+# export DB_DIR="$WORKFLOW_DIR/database/csv";
+
+# export PYTHONPATH=$WORKFLOW_DIR:$PYTHONPATH;
+
+# export TMPFILES_DIR_IV="/home/OUTPUT_ALI/TMPFILES_DIR_IV"
+
+# export SUMMARY_DIR_IV="/home/OUTPUT_ALI/SUMMARY_DIR_IV"
+
+# source $WORKFLOW_DIR/workflow_IV/setup.sh;
+
+# export TMPFILES_DIR_CV="/home/OUTPUT_ALI/TMPFILES_DIR_CV"
+
+# export SUMMARY_DIR_CV="/home/OUTPUT_ALI/SUMMARY_DIR_CV"
+
+# source $WORKFLOW_DIR/workflow_CV/setup.sh;
--- upload_testes_for_uploaded_wafers.py
+++ upload_testes_for_uploaded_wafers.py
@@ -1,11 +1,13 @@
-# import pandas as pd
+import pandas as pd
+import os
 wafers_file='CMS_HGCAL_DB/wafers_parts/HGCal Pre-series sensors - 120um HD_AUG26.csv'
+unzipped_parts_list_dir = 'CMS_HGCAL_DB/wafers_parts/wafers_parts_1'
 # preseries_tested = pd.read_csv('CMS_HGCAL_DB/wafers_parts/HGCal Pre-series sensors - 120um HD_AUG26.csv')
 # preseries_tested_scratchpad_id=preseries_tested['Scratch pad ID']
 # for ind, row in preseries_tested.iterrows():
 #     print(row['Scratch pad ID'])
 
-def return_uploaded_wafers_list(file):
+def return_uploaded_wafers_list_from_spreadsheet(file):
     with open(wafers_file, 'r') as f:
         sensor_id_l=[]; Scratchpad_id_l=[]
         f_readlines = f.readlines()
@@ -21,8 +23,16 @@ def return_uploaded_wafers_list(file):
                     begin_data_ind +=1
     return sensor_id_l, Scratchpad_id_l
 
+def return_uploaded_wafers_list_from_zip_file(directory):
+    Scratchpad_id_l=[]
+    for item in os.listdir(directory):
+        Scratchpad_id_l.append(item.split('.')[0])
+    return Scratchpad_id_l
+
 if __name__=='__main__':
-    sensor_id_l, Scratchpad_id_l = return_uploaded_wafers_list(wafers_file)
-    print(sensor_id_l)
-    print('\n\n')
-    print(Scratchpad_id_l)
+    # sensor_id_l, Scratchpad_id_l = return_uploaded_wafers_list_from_spreadsheet(wafers_file)
+    # print(sensor_id_l)
+    # print('\n\n')
+    # print(Scratchpad_id_l)
+    Scratchpad_id_l=return_uploaded_wafers_list_from_zip_file(unzipped_parts_list_dir)
+    print(sorted(Scratchpad_id_l))
