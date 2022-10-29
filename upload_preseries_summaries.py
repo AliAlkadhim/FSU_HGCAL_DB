3306f47699f8a898057956f8f19eb13e3dc4b648
AliAlkadhim <aa18dg@my.fsu.edu>
2022-10-29 12:15:41 -0500

    g

--- upload_preseries_summaries.py
+++ upload_preseries_summaries.py
@@ -1,4 +1,5 @@
 import os
+import TXT_TO_XML
 FSUDB_OUTPUT_DIR=os.environ['FSUDB_OUTPUT_DIR']
 
 summary_files_paths_file = 'CMS_HGCAL_DB/IV_CV_preseries_tested_at_FSU/preseries_fullpaths_no_duplicates.txt'
@@ -16,16 +17,19 @@ def upload_summary_files(IV_or_CV):
             if IV_or_CV=='IV':
                 if 'IV' in line:
                     fullpath=str(line)
-                    os.sytem('python TXT_TO_XML.py --f %s --t HGC_CERN_SENSOR_IV_SUMRY' % fullpath)
+                    # os.system('python TXT_TO_XML.py --f %s --t HGC_CERN_SENSOR_IV_SUMRY' % fullpath)
+                    TXT_TO_XML.make_xml_schema_HGC_CERN_SENSOR_IV_SUMRY(fullpath)
             elif IV_or_CV=='CV':
                 if 'CV' in line:
                     fullpath=str(line)
-                    os.sytem('python TXT_TO_XML.py --f %s --t HGC_CERN_SENSOR_CV_SUMRY' % fullpath)
+                    # os.system('python TXT_TO_XML.py --f %s --t HGC_CERN_SENSOR_CV_SUMRY' % fullpath)
+                    TXT_TO_XML.make_xml_schema_HGC_CERN_SENSOR_CV_SUMRY(fullpath)
+
         f.close()
                 
         
 
 
 if __name__ == '__main__':
-    upload_file('IV')
+    upload_summary_files('IV')
     
